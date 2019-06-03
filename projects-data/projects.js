c8a4d8d5cdff1f6ed5a9561948daa27747ebe0cd
Gonzalo Martinez <gonzarmv@gmail.com>
2019-06-03 14:06:51 -0700

    - working merge

---
+++ projects-data/projects.js
@@ -0,0 +1,181 @@
+const dataProjects = [
+  {
+    title: 'Middlebury College improved forest management project',
+    tagline:
+      'Located on 2,673.2 acres of mixed hardwood and conifer forest in the Adirondacks of central Vermont, the Middlebury College developed this project to become a carbon-neutral entity by offseting the carbon produced through the college’s operations.',
+    // main_picture: 'https://images.app.goo.gl/zVAMu3RXxmKeha3h9',
+    main_picture:
+      'https://i2.wp.com/vtdigger.org/wp-content/uploads/2015/07/Middlebury-campus.jpg?resize=610%2C413&ssl=1',
+    credits_avail: {
+      quatinty: 10000,
+      unit: 'credtis',
+    },
+    credits_issued: {
+      quatinty: 53884,
+      unit: 'total credits issued',
+    },
+    project_type: 'Improved Forest Management',
+    location: 'Middlebury, Vermont, USA',
+    // standard: 'https://drive.google.com/a/pachama.com/file/d/1gfnuWhb6-GRFH-pZi1Y70SThwSFQ_kh5/view?usp=sharing',
+    standard: 'https://americancarbonregistry.org/++theme++acr.plonetheme/static/logo.png',
+    project_summary:
+      'The Project improves forest management on Middlebury College’s forests, with Middlebury’s forest management practices representing an improvement in the carbon storage and conservation value compared to higher return management regimes of industrial private lands in the region, which are characterized by shorter, even-aged rotations. This project ensures the long-term conservation and sustainable management of the forest, promotes healthy wildlife habitat, and prevents future compromise of the forest carbon stocks. By committing to maintain forest CO2 stocks above the regional common practice, the project will provide significant climate benefits through carbon sequestration.',
+    total_land: {
+      area: 2673,
+      unit: 'acres',
+    },
+    last_verification: 'September 2018',
+    project_developer:
+      'https://drive.google.com/a/pachama.com/file/d/1HmFF7u7w40jDs6Tyxfkr5CBZrlEGpXek/view?usp=sharing',
+    auditor: 'https://drive.google.com/a/pachama.com/file/d/1XM9DYwYLSJMTQb5mb8ps4ZE-HJfMv8xi/view?usp=sharing',
+    methodology:
+      'IFM Methodology for Quantifying GHG Removals and Emission Reductions through Increased Forest Carbon Sequestration on Non‐Federal U.S. Forestlands v.1.2',
+    land_owner: 'https://drive.google.com/a/pachama.com/file/d/1vZCAO7PX7dcUb2sOjOo9p1GfdnVeHUhx/view?usp=sharing',
+    project_images: [
+      {
+        // image: 'https://images.app.goo.gl/EZ5MmGyyjddmz88i8',
+        image:
+          'https://s3-us-west-2.amazonaws.com/hobbyfarms.com/wp-content/uploads/2017/10/06175841/coniferous-trees-daniel-johnson.jpg',
+        caption: 'Coniferous trees is the more abundant specie on this project.',
+      },
+      {
+        // image: 'https://images.app.goo.gl/2UccJhzbn9nCj94TA',
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNRVBioRRB4Ej3Lff2m-sGbGK_rdkYw_lfIMC8er4YJjn6we67',
+        caption: 'Middlebury successfully reached its 2016 carbon neutrality goal.',
+      },
+    ],
+    badges: {
+      leakage: true,
+      additionality: true,
+      permanence: true,
+      verifiability: true,
+    },
+    summary: {
+      leakage:
+        'Middlebury College does not commercially harvest timber and nearly all forestlands owned by Middlebury College are included in the carbon project, therefore there is no activity-shifting leakage.',
+      additionality:
+        'Additionality for the project has been shown through a regulatory surplus test, a common practice test, and an implementation barrier test. Middlebury College committed to become a carbon neutral college by the end of 2017. Because Middlebury needs to meet its climate goals, if they do not develop the project, by developing a forest carbon offset project, they avoid the expenditures associated with purchasing credits from the market while generating additional revenue for the university without having to harvest timber.',
+      permanence:
+        "To ensure the permanence of the GHG emission reductions from this project, buffer credits from each of the projects are put on hold in a pool. When a loss event occurs (fire, diseases, pets, etc) buffer credits are taken from this pool. Based on the result of the risk assessment using the 'ACR Tool for Risk Analysis and Buffer Determination' this project will contribute to the buffer pool 18% of their issued credits.",
+      verifiability:
+        'An objective third party—someone other than Middlebury College and the project developer and —must be able to look at project data and confirm that the carbon reductions are real and credible. SCS Global Services  acted as the third-party verifier in this project, who determined the proper baseline for greenhouse gas reductions and verified that the reductions adhere to strict monitoring and reporting standards.',
+    },
+    timeline: [
+      {
+        date: 'May 2016',
+        text: 'Project start date',
+      },
+      {
+        date: 'Sept 2017',
+        text: '1st Verification started',
+      },
+      {
+        date: 'Sept 2018',
+        text: '1st Verification finished',
+      },
+      {
+        date: '2022',
+        text: 'Next issuance of credits',
+      },
+    ],
+    project_files:
+      'https://acr2.apx.com/mymodule/reg/TabDocuments.asp?r=111&ad=Prpt&act=update&type=PRO&aProj=pub&tablename=doc&id1=368',
+    un_goals: [
+      'https://drive.google.com/a/pachama.com/file/d/1gOmt_T83hADiYZtPY1LwFhzu3ttumXuM/view?usp=sharing',
+      'https://drive.google.com/a/pachama.com/file/d/1OoTnC36Zlw87kHvCuwZD5PGjdg6iyvn6/view?usp=sharing',
+      'https://drive.google.com/a/pachama.com/file/d/1GL3fpujkCEUTpyYDFfaxoA90dhyOR1p5/view?usp=sharing',
+    ],
+    tagline_footer:
+      'With every ton of carbon that is sold, trees are protected, partnerships are formed, and our living Earth is restored to help solve climate change.',
+  },
+  {
+    title: 'Great Mountain Forest improved forest management project',
+    tagline:
+      'The Great Mountain Forest is one of the nation’s oldest conservation legacy organizations, and the largest conservation easement held in southern New England.',
+    // main_picture: 'https://images.app.goo.gl/3LsHreeRqYnLLipP9',
+    main_picture:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZSlAF_xJeHs7ofrpi4DviN-_iHyb5d8n28pM2617QkMjM_dgE9Q',
+    credits_avail: {
+      quatinty: 0,
+      unit: 'credtis',
+    },
+    credits_issued: {
+      quatinty: 336146,
+      unit: 'total credits issued',
+    },
+    project_type: 'Improved Forest Management',
+    location: 'Norfolk, Connecticut, USA',
+    // standard: 'https://drive.google.com/a/pachama.com/file/d/1gfnuWhb6-GRFH-pZi1Y70SThwSFQ_kh5/view?usp=sharing',
+    standard: 'https://americancarbonregistry.org/++theme++acr.plonetheme/static/logo.png',
+    project_summary:
+      'The Great Mountain Forest corporation encompasses more than 6,000 acres of contiguous forestland in the towns of Norfolk and Canaan, Connecticut. The mission of the corporation is to promote the working forest as a renewable resource, as a habitat for wildlife and as a classroom for training and education. To do so they developed this project that will bring improvement for the flora and fauna of the forest. The main forest types across the property are mixed hardwoods, northern conifers, and northern hardwoods.',
+    total_land: {
+      area: 5898,
+      unit: 'acres',
+    },
+    last_verification: 'September 2017',
+    project_developer:
+      'https://drive.google.com/a/pachama.com/file/d/1HmFF7u7w40jDs6Tyxfkr5CBZrlEGpXek/view?usp=sharing',
+    auditor: 'https://drive.google.com/a/pachama.com/file/d/1XM9DYwYLSJMTQb5mb8ps4ZE-HJfMv8xi/view?usp=sharing',
+    methodology: 'ARB 2015 Compliance Offset Protocol for U.S. Forest Projects',
+    land_owner: 'https://drive.google.com/file/d/1NghYvDzNYdt7XROAAqpXClmAtHzsU17C/view',
+    project_images: [
+      {
+        image: 'http://www.greatmountainforest.org/admin/resources/harvestcuts-w640.jpg',
+        caption:
+          'Harvest cuts require the removal of mature trees to promote the establishment of a new generation of trees and increase the diversity.',
+      },
+      {
+        image: 'http://www.greatmountainforest.org/admin/resources/recreationfrankrec007.jpg',
+        caption: 'The Great Mountain Forest Corporation allows public access to the Great Mountain Forest.',
+      },
+    ],
+    badges: {
+      leakage: true,
+      additionality: true,
+      permanence: true,
+      verifiability: true,
+    },
+    summary: {
+      leakage: "TBD - Couldn't find any info on the Docs - We will ask Bluse Source about it :(",
+      additionality:
+        'The project harvest activities will employ uneven-aged silvicultural practices and maintain canopy retention averaging at least 40% across the forest, as measured on any 20 acres within the entire forestland owned by the Forest Owner, including land within and outside of the Project Area. Harvesting activity will maintain and increase carbon stocks well above the baseline. Baseline harvesting activities would include following Best Management Practices (BMPs) for Connecticut. No more than 10% of the total Forest Legacy easement area will be clear-cut within a 10-year period; and at least 50% of the crown canopy shall be maintained when 34 harvesting timber within 100 feet of a stream or water body.',
+      permanence:
+        "To ensure the permanence of the GHG emission reductions from this project, buffer credits from each of the projects are put on hold in a pool. When a loss event occurs (fire, diseases, pets, etc) buffer credits are taken from this pool. Based on the result of the risk assessment using the 'ACR Tool for Risk Analysis and Buffer Determination' this project will contribute to the buffer pool 17.6% of their issued credits.",
+      verifiability:
+        'An objective third party—someone other than Middlebury College and the project developer and —must be able to look at project data and confirm that the carbon reductions are real and credible. SCS Global Services  acted as the third-party verifier in this project, who determined the proper baseline for greenhouse gas reductions and verified that the reductions adhere to strict monitoring and reporting standards.',
+    },
+    timeline: [
+      {
+        date: 'March 2017',
+        text: 'Project start date',
+      },
+      {
+        date: 'March 2017',
+        text: '1st Verification started',
+      },
+      {
+        date: 'Sept 2017',
+        text: '1st Verification finished',
+      },
+      {
+        date: '2022',
+        text: 'Next verification and issuance of credits',
+      },
+      {
+        date: '2042',
+        text: 'End crediting period',
+      },
+    ],
+    project_files:
+      'https://acr2.apx.com/mymodule/reg/TabDocuments.asp?r=111&ad=Prpt&act=update&type=PRO&aProj=pub&tablename=doc&id1=371',
+    un_goals: [
+      'https://drive.google.com/a/pachama.com/file/d/1OoTnC36Zlw87kHvCuwZD5PGjdg6iyvn6/view?usp=sharing',
+      'https://drive.google.com/a/pachama.com/file/d/1GL3fpujkCEUTpyYDFfaxoA90dhyOR1p5/view?usp=sharing',
+    ],
+    tagline_footer:
+      'With every ton of carbon that is sold, trees are protected, partnerships are formed, and our living Earth is restored to help solve climate change.',
+  },
+];
+export default dataProjects;
