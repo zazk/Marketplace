3612e1c6a0235d9d05a1a22a741832b0de273836
Enrique Juan de Dios <ed@aerolab.co>
2019-06-25 19:40:30 -0500

    [45] remove /formreserve routes

--- routes.js
+++ routes.js
@@ -4,5 +4,4 @@ module.exports = routes()
   .add('home', '/', 'index')
   .add('map', '/map', 'map')
   .add('details', '/details', 'Details')
-  .add('formreserve', '/formreserve', 'FormReserve')
   .add('projectlist', '/projectlist', 'ProjectList');
