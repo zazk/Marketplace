3a85448f471e66d3c0b7080b77500d7999b6fbe2
Enrique Juan de Dios <ed@aerolab.co>
2019-07-01 09:43:49 -0500

    [53] remove home in routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const routes = require('next-routes');
 
 module.exports = routes()
-  .add('home', '/', 'index')
   .add('map', '/map', 'map')
   .add('details', '/details', 'Details')
   .add('projectlist', '/projectlist', 'ProjectList');
