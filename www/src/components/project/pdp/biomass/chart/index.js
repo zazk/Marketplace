ed69e35d664be6e49d08762242697762a2296fe6
Enrique Juan de Dios <ed@aerolab.co>
2019-08-15 07:36:50 -0500

    [68] refactor chart component

--- www/src/components/project/pdp/biomass/chart/index.js
+++ www/src/components/project/pdp/biomass/chart/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { VictoryChart, VictoryArea, VictoryTheme } from 'victory';
+import PropTypes from 'prop-types';
 
 function BiomassChart({ data }) {
-  const biomass = data;
+  const charts = [
+    {
+      fill: '#e9ecf2',
+      interpolation: 'natural',
+      biomass: '5',
+    },
+    {
+      fill: '#e9ecf2',
+      interpolation: 'natural',
+      biomass: '25',
+    },
+    {
+      fill: '#e9ecf2',
+      interpolation: 'natural',
+      biomass: '50',
+    },
+  ];
+
   return (
     <VictoryChart width={631} height={350} theme={VictoryTheme.material} animate={{ duration: 1000 }}>
-      <VictoryArea style={{ data: { fill: '#e9ecf2' } }} interpolation="natural" data={biomass['5']} />
-      <VictoryArea style={{ data: { fill: '#c1dfdb' } }} interpolation="natural" data={biomass['25']} />
-      <VictoryArea style={{ data: { fill: '#17b363' } }} interpolation="natural" data={biomass['50']} />
+      {charts.map(({ fill, interpolation, biomass }, i) => (
+        <VictoryArea key={i} style={{ data: { fill: fill } }} interpolation={interpolation} data={data[biomass]} />
+      ))}
     </VictoryChart>
   );
 }
+
+BiomassChart.propTypes = {
+  data: PropTypes.object,
+};
 export default BiomassChart;
