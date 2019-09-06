07d0a782363cbc1750ad4a6f5b080ac38b395c89
Enrique Juan de Dios <ed@aerolab.co>
2019-09-06 13:44:12 -0500

    [91] code cleaning: remove console.logs

--- www/src/components/common/user/state/operations.js
+++ www/src/components/common/user/state/operations.js
@@ -1,10 +1,9 @@
 import { requestCreateAccount, receiveCreateAccount } from './actions';
-
+import Const from '../../../../utils/const';
 export const submitRequestCreateAccount = data => {
-  console.log('submitRequestCreateAccountXXX');
   return dispatch => {
     dispatch(requestCreateAccount(data));
-    return fetch('http://localhost:3001/api/account', {
+    return fetch(`${Const.urlSite}api/account`, {
       method: 'post',
       headers: {
         'Content-type': 'application/json',
@@ -13,7 +12,6 @@ export const submitRequestCreateAccount = data => {
     })
       .then(response => response.json())
       .then(data => {
-        console.log('data desde operation', data);
         if (data.username) {
           dispatch(receiveCreateAccount(data));
         } else {
