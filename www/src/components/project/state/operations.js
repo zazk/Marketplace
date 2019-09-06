5060069e6c02226acd12399e34ab8911085a0482
Enrique Juan de Dios <ed@aerolab.co>
2019-09-06 13:51:30 -0500

    [91] Update url api in operation for Request Quote

--- www/src/components/project/state/operations.js
+++ www/src/components/project/state/operations.js
@@ -1,5 +1,5 @@
 import { requestProjects, receiveProjects, receiveForm, requestForm } from './actions';
-
+import Const from '../../../utils/const';
 export const fetchProjects = () => {
   return dispatch => {
     dispatch(requestProjects);
@@ -14,7 +14,7 @@ export const fetchProjects = () => {
 export const submitRequest = data => {
   return dispatch => {
     dispatch(requestForm(data));
-    return fetch(`http://localhost:3001/api/requestQuote`, {
+    return fetch(`${Const.urlSite}api/requestQuote`, {
       method: 'post',
       headers: {
         Accept: 'application/json, text/plain, */*',
