8debcd018b2259852e9ee86a6107c24b0ebc4c2e
Gonzalo Martinez <gonzarmv@gmail.com>
2019-09-11 14:51:15 -0700

    almost working

--- pages/maplist.js
+++ pages/maplist.js
@@ -1,4 +1,29 @@
 import React from 'react';
+import Layout from '../components/layout';
 import MapListVisualization from '../components/MapListVisualization';
+import PropTypes from 'prop-types';
 
-export default () => <MapListVisualization />;
+function ProjectMapList({ user }) {
+  return (
+    <Layout title="Project List" bodyclass="headerfull" user={user}>
+      <div className="map-list">
+        <div className="detail-wrap-map">
+          <MapListVisualization />
+        </div>
+        <style jsx global>
+          {`
+            .detail-wrap-map {
+              width: calc(100vw - 430px);
+            }
+          `}
+        </style>
+      </div>
+    </Layout>
+  );
+}
+
+ProjectMapList.propTypes = {
+  user: PropTypes.object,
+};
+
+export default ProjectMapList;
