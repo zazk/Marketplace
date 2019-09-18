7679634f1ab4f92d7f084e72a67f5f44b7f30406
Enrique Juan de Dioz <juandedioz@gmail.com>
2019-09-18 17:19:53 -0500

    [91] Code cleaned  of new pages

--- www/src/routes.js
+++ www/src/routes.js
@@ -4,13 +4,7 @@ import Projects from './pages/list';
 import Admin from './pages/admin';
 import CallBack from './pages/callback';
 import CreateAccount from './pages/createacount';
-import Explore from './pages/explore';
-import ExploreRequest from './pages/explorerequest';
-import MyCertificates from './pages/mycertificates';
-import ProjectsUser from './pages/project-user';
-import ProjectsCalculated from './pages/projectscalculated';
-import ProjectCalculatedEmpty from './pages/projects-calculated-empty';
-import ProjectNotCalculated from './pages/projects-notcalculated';
+
 import Dashboard from './pages/dashboard';
 
 const publicRoutes = [
@@ -48,48 +42,6 @@ const publicRoutes = [
     name: 'Create Account',
     component: CreateAccount,
     path: '/create-account',
-    exact: true,
-  },
-  {
-    name: 'Explore',
-    component: Explore,
-    path: '/explore',
-    exact: true,
-  },
-  {
-    name: 'Explore Request',
-    component: ExploreRequest,
-    path: '/explore-request',
-    exact: true,
-  },
-  {
-    name: 'My Certificates',
-    component: MyCertificates,
-    path: '/my-certificates',
-    exact: true,
-  },
-  {
-    name: 'ProjectsUser',
-    component: ProjectsUser,
-    path: '/projects-user',
-    exact: true,
-  },
-  {
-    name: 'Projects Calculated',
-    component: ProjectsCalculated,
-    path: '/projects-calculated',
-    exact: true,
-  },
-  {
-    name: 'Project Calculated Empty',
-    component: ProjectCalculatedEmpty,
-    path: '/project-calculated-empty',
-    exact: true,
-  },
-  {
-    name: 'Project Not Calculated',
-    component: ProjectNotCalculated,
-    path: '/project-not-calculated',
     exact: true,
   },
   {
